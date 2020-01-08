e286a9ec32faf126e3a87fd2649ce6ad53b83edf
mustafiz012 <mustafiz012@outlook.com>
2020-01-08 22:37:01 +0600

    ReadLine module added

--- app.js
+++ app.js
@@ -52,4 +52,35 @@ babai.emit('name');
 
 //==>>Readline module
 const readLine = require('readline');
-readLine.createInterface({input:process})
+let rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let num1 = Math.floor((Math.random() * 10 + 1));
+let num2 = Math.floor((Math.random() * 10 + 1));
+let answer = num1 + num2;
+
+let query = 'What is ' + num1 + ' + ' + num2 + '? \n';
+rl.question(query, (userInput) => {
+    if (userInput.trim() == answer) {
+        rl.close();
+    } else {
+        console.log('Incorrect: ' + userInput);
+        rl.setPrompt('Please try again\n');
+        rl.prompt();
+        rl.on('line', (userInput) => {
+            if (userInput.trim() == answer) {
+                rl.close();
+            } else {
+                console.log('Incorrect: ' + userInput);
+                rl.setPrompt('Please try again\n');
+                rl.prompt();
+            }
+        })
+    }
+});
+
+rl.on('close', () => {
+    console.log('Correct!!!');
+});
