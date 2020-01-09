7994f63a6ba954790f15fb5efd4f50eb1e5942ba
mustafiz.bracu <mustafizur.rahman@bracu.ac.bd>
2020-01-09 09:51:53 +0600

    File system module :: little more funcionalities added

--- fsm_app.js
+++ fsm_app.js
@@ -3,7 +3,7 @@ Working with file system module
 * */
 const fs = require('fs');
 //create file
-fs.writeFile('example.txt', "This is an example", (err) => {
+/*fs.writeFile('example.txt', "This is an example", (err) => {
     if (err) {
         console.log(err)
     } else {
@@ -15,7 +15,7 @@ fs.writeFile('example.txt', "This is an example", (err) => {
                 console.log(file)
         })
     }
-});
+});*/
 
 /*
 fs.rename('example.txt', 'example2.txt',
@@ -41,3 +41,92 @@ fs.unlink('example.txt', (err) => {
     else
         console.log('Successfully deleted the file')
 });*/
+
+// ==>>working with directory/folder
+//create a folder/dir
+/*fs.mkdir('utils', (err) => {
+    if (err)
+        console.log(err);
+    else {
+        //delete that folder/dir after successful creation
+        fs.rmdir('utils', (err)=>{
+            if (err)
+                console.log(err);
+            else
+                console.log('Successfully deleted the folder')
+        })
+    }
+});*/
+
+/*fs.mkdir('utils2', (err) => {
+    if (err)
+        console.log(err);
+    else {
+        console.log('Successfully created the folder')
+        //create a file inside folder after successful creation
+        fs.writeFile('./utils2/example.txt', 'Example file text', (err1 => {
+            if (err1)
+                console.log(err1)
+            else
+                console.log('File successfully created')
+        }))
+    }
+});*/
+//delete file and dir
+/*fs.unlink('./utils2/example.txt', err => {
+    if (err)
+        console.log(err);
+    else {
+        console.log('File deleted successfully');
+        fs.rmdir('utils2', err1 => {
+            if (err1)
+                console.log(err1);
+            else
+                console.log('Folder deleted successfully')
+        })
+    }
+});*/
+
+fs.mkdir('example', (err) => {
+    if (err) {
+        console.log(err);
+        fs.readdir('example', (err1, files) => {
+            for (let file of files) {
+                fs.unlink('./example/'+file, err2 => {
+                    if (err2)
+                        console.log(err2);
+                    else
+                        console.log('File deleted successfully')
+                })
+            }
+        });
+        fs.rmdir('example', err1 => {
+            if (err1)
+                console.log(err1);
+            else
+                console.log('example folder deleted successfully')
+        })
+    } else {
+        console.log('Successfully created the folder');
+        //create a file inside folder after successful creation
+        fs.writeFile('./example/a1.txt', 'a1 file text', (err1 => {
+            if (err1)
+                console.log(err1);
+            else
+                console.log('File successfully created')
+        }));
+        fs.writeFile('./example/a2.txt', 'a2 file text', (err1 => {
+            if (err1)
+                console.log(err1);
+            else
+                console.log('File successfully created')
+        }));
+
+        fs.readdir('example', (err1, files) => {
+            if (err1)
+                console.log(err1);
+            else
+                console.log(files)
+        })
+    }
+});
