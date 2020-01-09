c56f4c91802cd297adc9a70086b2e0ffbfc079f9
mustafiz.bracu <mustafizur.rahman@bracu.ac.bd>
2020-01-09 14:04:05 +0600

    Serving static files with http and file system modules :: image, video, json, html file serviing

--- http_server_app.js
+++ http_server_app.js
@@ -1,9 +1,9 @@
 // ==>> Creating a Http Server using Http module
 const http = require('http');
-const server = http.createServer((req, res) => {
+/*const server = http.createServer((req, res) => {
     //hello world for http server
-    /*res.write('Hello from server end');
-    res.end();*/
+    /!*res.write('Hello from server end');
+    res.end();*!/
 
     if (req.url === '/') {
         res.write('Hello from server end');
@@ -13,4 +13,15 @@ const server = http.createServer((req, res) => {
     res.end();
 });
 
-server.listen('3000');
+server.listen('3000');*/
+
+// ==>> Serving static files with http and file system modules
+const fs = require('fs');
+http.createServer((req, res) => {
+    const readStream = fs.createReadStream('./static/xb_dancing.mp4');
+    // res.writeHead(200, {'Content-type': 'text/html'});
+    // res.writeHead(200, {'Content-type': 'application/json'});
+    // res.writeHead(200, {'Content-type': 'image/jpeg'});
+    res.writeHead(200, {'Content-type': 'video/mp4'});
+    readStream.pipe(res);
+}).listen(3000);
