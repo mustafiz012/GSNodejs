0c6a262f14c5bd53bb722e981bc50b7dad2971e2
mustafiz.bracu <mustafizur.rahman@bracu.ac.bd>
2020-01-09 16:01:26 +0600

    Express web framework introduced with GET request

--- package_npm_app.js
+++ package_npm_app.js
@@ -1,5 +1,20 @@
 // ==>> Creating package.json using npm init
-const _=require('lodash');
-let example = _.fill([1,2,3,4,5], "banana",3,5);
+/*const _ = require('lodash');
+let example = _.fill([1, 2, 3, 4, 5], "banana", 3, 5);
 
-console.log(example);
+console.log(example);*/
+
+// ==>> Getting started with Express framework (Web framework)
+const express = require('express');
+const app = express();
+
+//GET request
+app.get('/', (req, res) => {
+    res.send('Hello from express root route')
+});
+
+app.get('/example', (req, res)=>{
+    res.send('This is /example route')
+});
+
+app.listen(3000);
